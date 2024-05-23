403188f3a6897228423d1195dbf912375c98e320
Mani Soundararajan <root@msound.net>
2024-05-23 01:06:54 -0400

    feat: print hello world

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/msound/example-helloworld
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "log"
+
+const appVersion = "v0.0.1"
+
+func main() {
+	log.Println("Helloworld", appVersion)
+}
